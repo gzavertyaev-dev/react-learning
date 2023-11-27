7b56fcd1354c9e4ce35b8a6448ab47c7e18e4c61
ppavlov <ppavlov.frontend@gmail.com>
2023-11-27 21:59:09 +0100

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import * as React from 'react';
 import "./App.css";
 import Lesson18 from "./lessons/Lesson18";
 
 function App() {
   return (
     <div className="App">
-      {/* Использовать Lessons только во время обучения,
-      когда начнем писать проект, такой структуры у нас не будет */}
       <Lesson18 />
     </div>
   );
