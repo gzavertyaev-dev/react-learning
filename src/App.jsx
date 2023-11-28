9595b60ed0b4a98c466f644d97ca7fa7e92f01b7
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2023-11-28 10:18:24 +0100

    Add lesson 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import Lesson18 from "./lessons/Lesson18";
+// import Lesson18 from "./lessons/Lesson18";
+import HomeWork18 from "./homeworks/Homework18";
 
 function App() {
   return (
     <div className="App">
       {/* Использовать Lessons только во время обучения,
       когда начнем писать проект, такой структуры у нас не будет */}
-      <Lesson18 />
+      {/* <Lesson18 /> */}
+      <HomeWork18 />
     </div>
   );
 }
---
+++ src/homeworks/Homework18/HomeWork18.jsx
@@ -0,0 +1,7 @@
+import Counter from "../../components/Counter";
+
+function HomeWork18() {
+  return <Counter />;
+}
+
+export default HomeWork18;
